fb513153f9a816011ee6cf29ce51d43d1028d053
Karina <litvina.k@ukr.net>
2022-09-02 12:39:02 +0300

    new

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
+let start = parseInt(prompt('Enter your first number'), 10);
 
-
-
-
-
-let n = parseInt(prompt('Enter your first number'), 10);
-
-if (isNaN(n)) {
-    n = parseInt(prompt('Enter number again'), 10)
+if (isNaN(start)) {
+    start = parseInt(prompt('Enter number again'), 10)
 }
 
-let m = parseInt(prompt('Enter your second number'), 10);
+let end = parseInt(prompt('Enter your second number'), 10);
 
-if (isNaN(m)) {
-    m = parseInt(prompt('Enter number again'), 10)
+if (isNaN(end)) {
+    end = parseInt(prompt('Enter number again'), 10)
 }
 
 let skipPairNumbers = confirm("Need to skip pairs?");
 
 let sum = 0;
 
-for (let i = n; i <= m; i++) {
+for (let i = start; i <= end; i++) {
     if (!skipPairNumbers) {
          sum = sum + i;
          
     }  else {
-        if (i % 2 !== 0) {
+        let isEven = i % 2 !== 0
+        if(skipPairNumbers && isEven) {
             sum = sum + i;
     }   
 }
